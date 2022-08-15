1f13b8ed5f2f867209096072e822c666d930c9c1
PabloLogioco <pablologioco@yahoo.com.ar>
2022-08-15 10:34:17 -0300

    Presenta Maquetado Modulo 2

---
+++ Main.js
@@ -0,0 +1,86 @@
+//
+console.log("Main.JS activo");
+
+LeerDatosTitular();
+
+LeerDatosExperiencia();
+
+LeerDatosEstudios();
+
+LeerDatosProyectos();
+
+/////////////////////////////////////////////////////////////////
+//
+//                  FUNCIONES
+//
+/////////////////////////////////////////////////////////////////
+
+function LeerDatosTitular(){
+    let datos_titular = document.querySelector('#titular');
+    datos_titular.innerHTML = ''; /// pone en blanco, cero
+
+    for(let item of array_titular) { // recorre array
+        datos_titular.innerHTML += `  
+            <h6 >${item.nombre}</h6>
+            <h6>${item.titulo}</h6> ` /// comillas especiales, ALT 96
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+
+function LeerDatosEstudios(){
+
+    let datos_estudios = document.querySelector('#datos_estudios');
+    datos_estudios.innerHTML = ''; /// pone en blanco, cero
+
+    for(let item of array_estudios) { // recorre array
+        datos_estudios.innerHTML += `  
+        <tr>
+            <td>${item.anio}</td>
+            <td>${item.institucion}</td>
+            <td>${item.descripcion}</td>
+            <button class="btn btn-outline-primary btn-sm" style=display:none>Edit</button>
+        </tr> ` /// comillas especiales, ALT 96
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+function LeerDatosProyectos(){
+
+    let datos_proyectos = document.querySelector('#datos_proyectos');
+    datos_proyectos.innerHTML = ''; /// pone en blanco, cero
+
+    for(let item of array_proyectos) { // recorre array
+        datos_proyectos.innerHTML += `  
+        <tr>
+            <td>${item.anio}</td>
+            <td>${item.descripcion}</td>
+            <button class="btn btn-outline-primary btn-sm" style=display:none>Edit</button>
+        </tr>  ` /// comillas especiales, ALT 96
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+    
+function LeerDatosExperiencia(){
+
+    let datos_experiencia = document.querySelector('#datos_experiencia');
+    datos_experiencia.innerHTML = ''; /// pone en blanco, cero
+
+    for(let item of array_experiencia) { // recorre array
+        datos_experiencia.innerHTML += `  
+            <tr>
+                <td>${item.desde}</td>
+                <td>${item.hasta}</td>
+                <td>${item.empresa}</td>
+                <td>${item.descripcion}</td>
+                <button class="btn btn-outline-primary btn-sm" style=display:none>Edit</button>
+            </tr>  ` /// comillas especiales, ALT 96
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
